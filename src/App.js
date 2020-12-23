5bafee5f96efb5bf483e6995e87f9c4d0a509aa1
dotrider <56666461+dotrider@users.noreply.github.com>
2020-12-22 22:33:18 -0700

    Fetch products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React, { useState, useEffect } from 'react';
 import '../src/Components/Products/Products';
 import { Nav, Products } from './Components';
-import { Commerce } from './lib/Commerce';
+import { commerce } from './lib/Commerce';
 
 import './App.css';
 
 function App() {
+
+  const [ products, setProducts ] = useState([])
+
+
+  const getProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data)
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  console.log('products', products)
   return (
     <div className="App">
       <Nav/>
